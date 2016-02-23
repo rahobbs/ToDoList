7899308adc9359d9aca9d554cf138f6e12099690
Rachael Hobbs <hobbs.rachael@gmail.com>
2016-02-23 12:50:50 -0500

    Modified list view layout (moved checkboxes to left) and removed auto-generated case numbers in favor of adding version number to feedback email subject.

--- app/src/main/java/com/rahobbs/todo/Feedback.java
+++ app/src/main/java/com/rahobbs/todo/Feedback.java
@@ -1,7 +1,6 @@
 package com.rahobbs.todo;
 import android.content.Intent;
-
-import java.util.Random;
+import android.util.Log;
 
 /**
  * Created by rachael on 2/22/16.
@@ -11,13 +10,10 @@ public class Feedback {
 
 
     public Intent sendFeedback(Intent i) {
-        int rand = new Random().nextInt(301);
-        String versionCode = String.valueOf(BuildConfig.VERSION_CODE);
-        int ticketCode = rand + 100;
-
         i.setType("message/rfc822");
         i.putExtra(Intent.EXTRA_EMAIL, new String[]{"feedback@rahobbs.com"});
-        i.putExtra(Intent.EXTRA_SUBJECT, "(Case " + versionCode + ticketCode + ") The Round File Customer Feedback");
+        i.putExtra(Intent.EXTRA_SUBJECT, "The Round File Customer Feedback, version " + BuildConfig.VERSION_NAME);
+        Log.v("Confirm sendFeeback", "The Round File Customer Feedback, version " + BuildConfig.VERSION_NAME );
 
         return i;
 
