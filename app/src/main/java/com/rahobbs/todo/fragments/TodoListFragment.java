1c4fac16c94e64dbef458b6ed6cc585707d18786
rachael <hobbs.rachael@gmail.com>
2016-04-26 13:53:47 -0400

    Setting up Contextual Action Bar

--- app/src/main/java/com/rahobbs/todo/fragments/TodoListFragment.java
+++ app/src/main/java/com/rahobbs/todo/fragments/TodoListFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -180,6 +181,7 @@ public class TodoListFragment extends Fragment {
                 public boolean onLongClick(View arg0) {
                     selectedItems.add(mTodo);
                     multiSelectMode = true;
+                    getActivity().startActionMode(mActionModeCallback);
                     mListItem.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.done_task));
 
                     return true;    // <- set to true
@@ -320,4 +322,44 @@ public class TodoListFragment extends Fragment {
             return true;
         }
     }
+
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+        // Called when the action mode is created; startActionMode() was called
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Inflate a menu resource providing context menu items
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+            return true;
+        }
+
+        // Called each time the action mode is shown. Always called after onCreateActionMode, but
+        // may be called multiple times if the mode is invalidated.
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false; // Return false if nothing is done
+        }
+
+        // Called when the user selects a contextual menu item
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.multi_delete:
+                    deleteSelected(selectedItems);
+                    multiSelectMode = false;
+                    updateUI();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        // Called when the user exits the action mode
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            multiSelectMode = false;
+        }
+    };
 }
