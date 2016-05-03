efee2c7500f918444e3662439b2e5adff93af48a
rachael <hobbs.rachael@gmail.com>
2016-05-03 15:24:35 -0400

    Setting up Robolectric for testing. Currently running into issues running tests due to issue at https://github.com/robolectric/robolectric/issues/1329#issuecomment-215158741

---
+++ app/src/test/java/com/rahobbs/todo/TodoListFragmentTest.java
@@ -0,0 +1,27 @@
+package com.rahobbs.todo;
+
+import android.os.Build;
+
+import com.rahobbs.todo.fragments.TodoListFragment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.KITKAT)
+
+
+public class TodoListFragmentTest {
+
+    @Test
+    public void shouldNotBeNull() throws Exception
+    {
+        TodoListFragment fragment = new TodoListFragment();
+        startFragment( fragment );
+        assertNotNull( fragment );
+    }
+}
